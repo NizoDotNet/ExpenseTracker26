3bc690454e388429aabd70a8398588ec20c3ad20
NizoDotNet <mmmdovnizami.official@gmail.com>
2026-03-22 21:42:34 +0400

    need add migration

---
+++ ExpenseTracker/Infrastracture/Configurations/TransactionCategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.Domain.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseTracker.Infrastracture.Configurations;
+
+public class TransactionCategoryConfiguration : IEntityTypeConfiguration<TransactionCategory>
+{
+    public void Configure(EntityTypeBuilder<TransactionCategory> builder)
+    {
+        builder.Property(c => c.Name)
+            .HasMaxLength(50);
+    }
+}
